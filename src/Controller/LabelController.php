f448842ca0a91d3de1bb5d54b305a3a39407f1ef
MilesMak <yselim1@hotmail.com>
2019-12-05 17:03:09 +0100

    Réalisation des pages artistes, records et labels

--- src/Controller/LabelController.php
+++ src/Controller/LabelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Label;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,12 +14,10 @@ class LabelController extends AbstractController
     /**
      * @Route("/{id}", name="page")
      */
-    public function index($id)
+    public function index(Label $label)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/LabelController.php',
-            'id' => $id,
+        return $this->render('label/label_page.html.twig', [
+            'label' => $label
         ]);
     }
 }
